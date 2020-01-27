4e87b5cb84a21852344b1f35fe7e896dafa294ab
Mustafa <mustafalani@gmail.com>
2020-01-27 12:54:38 +0100

    change logs date format

--- scripts/vtrunkd.sh
+++ scripts/vtrunkd.sh
@@ -6,30 +6,30 @@ vprofile='000003_2'
 fping -c1 -t300 $vhost 2>/dev/null 1>/dev/null
 if [ "$?" = 0 ]
 then
-  echo "$(date '+%A %W %Y %X')	All Good" >> /home/iohub/iohub/logs/all/vtrunkd.log
+  echo "$(date '+%A %d-%m-%Y %X')	All Good" >> /home/iohub/iohub/logs/all/vtrunkd.log
 elif pgrep -x vtrunkd >/dev/null
-then echo "$(date '+%A %W %Y %X')	Restarting vtrunkd" >> /home/iohub/iohub/logs/all/vtrunkd.log
+then echo "$(date '+%A %d-%m-%Y %X')	Restarting vtrunkd" >> /home/iohub/iohub/logs/all/vtrunkd.log
 pkill -9 vtrunkd
 sleep 2
 sudo vtrunkd -P $vport -f /etc/vtrunkd.conf $vprofile $phost
-echo "$(date '+%A %W %Y %X')	vtrunkd has started" >> /home/iohub/iohub/logs/all/vtrunkd.log
+echo "$(date '+%A %d-%m-%Y %X')	vtrunkd has started" >> /home/iohub/iohub/logs/all/vtrunkd.log
 sleep 2
 fping -c1 -t300 $vhost 2>/dev/null 1>/dev/null
   if [ "$?" = 0 ]
   then
-    echo "$(date '+%A %W %Y %X')	All Good Again" >> /home/iohub/iohub/logs/all/vtrunkd.log
+    echo "$(date '+%A %d-%m-%Y %X')	All Good Again" >> /home/iohub/iohub/logs/all/vtrunkd.log
   else
-    echo "$(date '+%A %W %Y %X')	Something went wrong we will try again later.." >> /home/iohub/iohub/logs/all/vtrunkd.log
+    echo "$(date '+%A %d-%m-%Y %X')	Something went wrong we will try again later.." >> /home/iohub/iohub/logs/all/vtrunkd.log
   fi
 else
-  echo "$(date '+%A %W %Y %X')	Starting vtrunkd" >> /home/iohub/iohub/logs/all/vtrunkd.log
+  echo "$(date '+%A %d-%m-%Y %X')	Starting vtrunkd" >> /home/iohub/iohub/logs/all/vtrunkd.log
   sudo vtrunkd -P $vport -f /etc/vtrunkd.conf $vprofile $phost
   sleep 2
   fping -c1 -t300 $vhost 2>/dev/null 1>/dev/null
   if [ "$?" = 0 ]
   then
-    echo "$(date '+%A %W %Y %X')	All Good" >> /home/iohub/iohub/logs/all/vtrunkd.log
+    echo "$(date '+%A %d-%m-%Y %X')	All Good" >> /home/iohub/iohub/logs/all/vtrunkd.log
   else
-    echo "$(date '+%A %W %Y %X')	Something went wrong we will try again later.." >> /home/iohub/iohub/logs/all/vtrunkd.log
+    echo "$(date '+%A %d-%m-%Y %X')	Something went wrong we will try again later.." >> /home/iohub/iohub/logs/all/vtrunkd.log
   fi
 fi
