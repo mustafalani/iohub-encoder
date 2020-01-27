50ddd21d319bcd0db191f18e658b1a8b86ed1181
Mustafa <mustafalani@gmail.com>
2020-01-27 13:34:33 +0100

    vtrunkd logs

--- scripts/vtrunkd.sh
+++ scripts/vtrunkd.sh
@@ -3,29 +3,33 @@ phost='152.115.45.143'
 vhost='10.147.18.1'
 vport='5001'
 vprofile='000003_2'
-fping -c1 -t300 $vhost 2>/dev/null 1>/dev/null
+echo "$(date '+%A %d-%m-%Y %X')	Examing connection" >> /home/iohub/iohub/logs/all/vtrunkd.log
+fping -c1 -t300 $vhost >> /home/iohub/iohub/logs/all/vtrunkd.log
 if [ "$?" = 0 ]
 then
   echo "$(date '+%A %d-%m-%Y %X')	All Good" >> /home/iohub/iohub/logs/all/vtrunkd.log
-elif pgrep -x vtrunkd >/dev/null
+elif pgrep -x vtrunkd >> /home/iohub/iohub/logs/all/vtrunkd.log
 then echo "$(date '+%A %d-%m-%Y %X')	Restarting vtrunkd" >> /home/iohub/iohub/logs/all/vtrunkd.log
-pkill -9 vtrunkd
-sleep 2
-sudo vtrunkd -P $vport -f /etc/vtrunkd.conf $vprofile $phost
-echo "$(date '+%A %d-%m-%Y %X')	vtrunkd has started" >> /home/iohub/iohub/logs/all/vtrunkd.log
-sleep 2
-fping -c1 -t300 $vhost 2>/dev/null 1>/dev/null
+pkill -9 vtrunkd >> /home/iohub/iohub/logs/all/vtrunkd.log
+sleep 5
+sudo vtrunkd -P $vport -f /etc/vtrunkd.conf $vprofile $phost >> /home/iohub/iohub/logs/all/vtrunkd.log
+sleep 3
+echo "$(date '+%A %d-%m-%Y %X')	vtrunkd started..." >> /home/iohub/iohub/logs/all/vtrunkd.log
+echo "$(date '+%A %d-%m-%Y %X')	Examing connection after restart" >> /home/iohub/iohub/logs/all/vtrunkd.log
+fping -c1 -t300 $vhost >> /home/iohub/iohub/logs/all/vtrunkd.log
   if [ "$?" = 0 ]
   then
     echo "$(date '+%A %d-%m-%Y %X')	All Good Again" >> /home/iohub/iohub/logs/all/vtrunkd.log
   else
-    echo "$(date '+%A %d-%m-%Y %X')	Something went wrong we will try again later.." >> /home/iohub/iohub/logs/all/vtrunkd.log
+    echo "$(date '+%A %d-%m-%Y %X')	Something went wrong we will try again later..." >> /home/iohub/iohub/logs/all/vtrunkd.log
   fi
 else
   echo "$(date '+%A %d-%m-%Y %X')	Starting vtrunkd" >> /home/iohub/iohub/logs/all/vtrunkd.log
-  sudo vtrunkd -P $vport -f /etc/vtrunkd.conf $vprofile $phost
-  sleep 2
-  fping -c1 -t300 $vhost 2>/dev/null 1>/dev/null
+  sudo vtrunkd -P $vport -f /etc/vtrunkd.conf $vprofile $phost >> /home/iohub/iohub/logs/all/vtrunkd.log
+  sleep 3
+  echo "$(date '+%A %d-%m-%Y %X')	vtrunkd started.." >> /home/iohub/iohub/logs/all/vtrunkd.log
+  echo "$(date '+%A %d-%m-%Y %X')	Examing connection.." >> /home/iohub/iohub/logs/all/vtrunkd.log
+  fping -c1 -t300 $vhost >> /home/iohub/iohub/logs/all/vtrunkd.log
   if [ "$?" = 0 ]
   then
     echo "$(date '+%A %d-%m-%Y %X')	All Good" >> /home/iohub/iohub/logs/all/vtrunkd.log
